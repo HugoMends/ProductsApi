1a1feb6661cb20a0b2bf6119f883c8ed95a0aa72
HugoMends <hugo.mmoreira@outlook.com>
2025-03-24 21:11:41 -0300

    transactional import fixed

--- src/main/java/com/DevHugo/ProductsApi/services/ProductService.java
+++ src/main/java/com/DevHugo/ProductsApi/services/ProductService.java
@@ -4,12 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.DevHugo.ProductsApi.dto.ProductDTO;
 import com.DevHugo.ProductsApi.entity.Product;
 import com.DevHugo.ProductsApi.repositories.ProductsRepository;
 
-import jakarta.transaction.Transactional;
+
 
 @Service
 public class ProductService {
